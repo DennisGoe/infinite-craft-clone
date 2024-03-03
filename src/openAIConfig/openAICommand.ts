945dc0d17311fe388706e630d077ad5363f263eb
dgoerickeSP <d.goericke@systempiloten.de>
2024-03-03 18:49:56 +0100

    refined command

--- src/openAIConfig/openAICommand.ts
+++ src/openAIConfig/openAICommand.ts
@@ -1,6 +1,5 @@
 export const openAICommand = (firstString: string, secondString: string) => {
-    return `I will ask you one question. And i want you to asnwer this quesion with only one word!
+    return `I will ask you one question. And i want you to answer this question with only one word!
     What will you get if you put together these two things: ${firstString} + ${secondString}. Do not add 
-    any punctuation marks but only the answer to the question. If necessary you might answer with up two three words,
-    but you should try to only use one word. Make sure there is not a "." at the end of the word`
+    any punctuation marks but only the answer to the question. Make sure there is not a "." at the end of the word`
 }
